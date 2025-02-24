e4440fb9ed8f7301eac8dbfa031ca25b444e2ead
william felix <angra@Mac.fritz.box>
2025-02-24 11:14:25 +0100

    added variables

--- script.js
+++ script.js
@@ -22,6 +22,16 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says Store.";
+  }
+
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
