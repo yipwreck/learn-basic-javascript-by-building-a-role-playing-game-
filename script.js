b3a756e1d01e1786eb1af5eadcc1a5f1277086ae
william felix <angra@Mac.fritz.box>
2025-02-28 09:38:06 +0100

    added variables

--- script.js
+++ script.js
@@ -164,6 +164,8 @@ function attack() {
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= monsters[fighting].level;
   monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function dodge() {
