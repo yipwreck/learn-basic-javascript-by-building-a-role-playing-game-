eccf5b1ef72c410227f9ec9264ef27975bbfcb26
william felix <angra@Mac.fritz.box>
2025-03-02 21:53:55 +0100

    added variables

--- script.js
+++ script.js
@@ -198,6 +198,10 @@ function attack() {
       defeatMonster();
     }
   }
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeaponIndex--;
+  }
 }
 
 function getMonsterAttackValue(level) {
